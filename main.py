8edac2df15bae5c5f7a0a71d3063989a3e0dab70
ariel415el <ariel415el@gmail.com>
2023-02-26 16:25:16 +0200

    Refactor args and debug arg; Avoid opencv

--- main.py
+++ main.py
@@ -6,53 +6,62 @@ from patch_swd import PatchSWDLoss
 from utils import load_image, dump_images, get_pyramid_scales
 import argparse
 
-# IO
-parser = argparse.ArgumentParser(description='Run GPDM')
-parser.add_argument('target_image', help="This image has the reference patch distribution to be matched")
-parser.add_argument('--output_dir', default="Outputs", help="Where to put the results")
-parser.add_argument('--debug_dir', default=None, help="If not None, debug images are dumped to this path")
 
-# SWD parameters
-parser.add_argument('--patch_size', type=int, default=7)
-parser.add_argument('--stride', type=int, default=1)
-parser.add_argument('--num_proj', type=int, default=64, help="Number of random projections used to approximate SWD")
+def parse_args():
+    # IO
+    parser = argparse.ArgumentParser(description='Run GPDM')
+    parser.add_argument('target_image', help="This image has the reference patch distribution to be matched")
+    parser.add_argument('--output_dir', default="Outputs", help="Where to put the results")
+    parser.add_argument('--debug', action='store_true', default=False, help="Dumpp debug images")
 
-# Pyramids parameters
-parser.add_argument('--fine_dim', type=int, default=None,
-                    help="Height of the largest ptramid scale (can be used to get smaller output)."
-                    "If None use the target_image height")
-parser.add_argument('--coarse_dim', type=int, default=21,
-                    help="Height of the smallest pyramid scale, When starting from noise,"
-                    " bigger coarse dim lets the images outputs go more diverse (coarse_dim==~patch_size) "
-                    "will probably output a copy the input")
-parser.add_argument('--pyr_factor', type=float, default=0.85, help="Downscale factor of the pyramid")
-parser.add_argument('--height_factor', type=float, default=1., help="Controls the aspect ratio of the result: factor of height")
-parser.add_argument('--width_factor', type=float, default=1., help="Controls the aspect ratio of the result: factor of width")
+    # SWD parameters
+    parser.add_argument('--patch_size', type=int, default=7)
+    parser.add_argument('--stride', type=int, default=1)
+    parser.add_argument('--num_proj', type=int, default=64, help="Number of random projections used to approximate SWD")
 
-# GPDM parameters
-parser.add_argument('--init_from', default='zeros', help="Defines the intial guess for the first level. Can one of ('zeros', 'target', '<path-to-image>')")
-parser.add_argument('--lr', type=float, default=0.01, help="Adam learning rate for the optimization")
-parser.add_argument('--num_steps', type=int, default=300, help="Number of Adam steps")
-parser.add_argument('--noise_sigma', type=float, default=1.5, help="Std of noise added to the first initial image")
-parser.add_argument('--num_images', type=int, default=1, help="If > 1, batched inference is used (see paper) and multiple images are generated")
+    # Pyramids parameters
+    parser.add_argument('--fine_dim', type=int, default=None,
+                        help="Height of the largest ptramid scale (can be used to get smaller output)."
+                             "If None use the target_image height")
+    parser.add_argument('--coarse_dim', type=int, default=21,
+                        help="Height of the smallest pyramid scale, When starting from noise,"
+                             " bigger coarse dim lets the images outputs go more diverse (coarse_dim==~patch_size) "
+                             "will probably output a copy the input")
+    parser.add_argument('--pyr_factor', type=float, default=0.85, help="Downscale factor of the pyramid")
+    parser.add_argument('--height_factor', type=float, default=1.,
+                        help="Controls the aspect ratio of the result: factor of height")
+    parser.add_argument('--width_factor', type=float, default=1.,
+                        help="Controls the aspect ratio of the result: factor of width")
 
-args = parser.parse_args()
+    # GPDM parameters
+    parser.add_argument('--init_from', default='zeros',
+                        help="Defines the intial guess for the first level. Can one of ('zeros', 'target', '<path-to-image>')")
+    parser.add_argument('--lr', type=float, default=0.01, help="Adam learning rate for the optimization")
+    parser.add_argument('--num_steps', type=int, default=300, help="Number of Adam steps")
+    parser.add_argument('--noise_sigma', type=float, default=1.5, help="Std of noise added to the first initial image")
+    parser.add_argument('--num_images', type=int, default=1,
+                        help="If > 1, batched inference is used (see paper) and multiple images are generated")
+
+    return parser.parse_args()
 
 
 if __name__ == '__main__':
+    args = parse_args()
+
     refernce_images = load_image(args.target_image).repeat(args.num_images, 1, 1, 1)
 
     criteria = PatchSWDLoss(patch_size=args.patch_size, stride=args.stride, num_proj=args.num_proj)
 
     fine_dim = args.fine_dim if args.fine_dim is not None else refernce_images.shape[-2]
 
-    new_iamges = GPDM.generate(refernce_images, criteria,
+    outputs_dir = join(args.output_dir, basename(args.target_image))
+    new_images = GPDM.generate(refernce_images, criteria,
                                pyramid_scales=get_pyramid_scales(fine_dim, args.coarse_dim, args.pyr_factor),
                                aspect_ratio=(args.height_factor, args.width_factor),
                                init_from=args.init_from,
                                lr=args.lr,
                                num_steps=args.num_steps,
                                additive_noise_sigma=args.noise_sigma,
-                               debug_dir=args.debug_dir
+                               debug_dir=f"{outputs_dir}/debug" if args.debug else None
     )
-    dump_images(new_iamges, join(args.output_dir, basename(args.target_image)))
+    dump_images(new_images, outputs_dir)
--- utils.py
+++ utils.py
@@ -1,6 +1,6 @@
 import os
 
-import cv2
+from PIL import Image
 import numpy as np
 import torch
 from matplotlib import pyplot as plt
@@ -8,7 +8,15 @@ from torchvision.utils import save_image
 
 
 def load_image(path):
-    return cv2pt(cv2.imread(path))
+    img = np.array(Image.open(path))
+    img = img - img.min()
+    img = img / img.max()
+    img = img * 2 - 1
+    if len(img.shape) == 2:
+        img = img[..., None]
+    img = img.transpose(2, 0, 1)
+    img = torch.from_numpy(img).unsqueeze(0).float()
+    return img
 
 
 def dump_images(images, out_dir):
@@ -20,6 +28,16 @@ def dump_images(images, out_dir):
     for j in range(images.shape[0]):
         save_image(images[j], os.path.join(out_dir, f"{i}.png"), normalize=True)
         i += 1
+
+def show_nns(images, ref_images, out_dir):
+    nn_indices = []
+    for i in range(len(images)):
+        dists = torch.mean((ref_images - images[i].unsqueeze(0))**2, dim=(1,2,3))
+        j = dists.argmin()
+        nn_indices.append(j)
+
+    debug_image = torch.cat([images, ref_images[nn_indices]], dim=0)
+    save_image(debug_image, os.path.join(out_dir, f"NNs.png"), normalize=True, nrow=len(ref_images))
 
 
 def get_pyramid_scales(max_height, min_height, step):
@@ -35,18 +53,10 @@ def get_pyramid_scales(max_height, min_height, step):
     return scales[::-1]
 
 
-def cv2pt(img):
-    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-    img = img.astype(np.float64) / 255.
-    img = img * 2 - 1
-    img = torch.from_numpy(img.transpose(2, 0, 1)).float().unsqueeze(0)
-
-    return img
-
-
 def plot_loss(losses, path):
     os.makedirs(os.path.dirname(path), exist_ok=True)
     fig1 = plt.figure()
+    fig1.suptitle(f'Last loss: {losses[-1]}')
     ax = fig1.add_subplot(111)
     ax.plot(np.arange(len(losses)), losses)
     # ax.plot(np.arange(len(losses)), np.log(losses))
